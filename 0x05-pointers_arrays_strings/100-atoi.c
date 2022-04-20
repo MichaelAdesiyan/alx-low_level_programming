e9df0167c1a7c83577f1b113d04491415650fbe9
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-04-20 13:54:37 -0700

    great leaders

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s: char type string 
+ * Return: integer converted
+ */
+int _atoi (char *s)
+{
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
+	for (1 = 0; s[i] != '\0';i++)
+	{
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9'
+		{
+			res = res *10;
+			res -= (s[i] - '0');
+		}
+		else if (brk == 1)
+			break;
+	}
+	res = sig * res;
+	return (res);
+}
