0ce0f8b2f01af9292b1ecc274abff4414a3e696e
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-05-24 20:40:55 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,6 @@
 void_attribute_((constructor)) print_first(void);
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\n);
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
