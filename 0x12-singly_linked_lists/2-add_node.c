dbcc291cae49d4499a2f7b580f34b3110f246a33
MichaelAdesiyan <99549113+MichaelAdesiyan@users.noreply.github.com>
2022-05-27 04:20:34 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,16 +1,43 @@
 #include "lists.h"
-listint_t *add_nodeint(listint_t **head, const int n)
+
+/**
+ * _strlen - find string length
+ * @str: string
+ * Return: length
+ */
+int _strlen(const char *str)
 {
-	listint_t *new;
+	int i;
 
-	new = malloc(sizeof(listint_t));
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
-	if (new == NULL)
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+	if (strdup(str) == NULL)
 		return (NULL);
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-	return (*head);
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+
+	if (head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
 }
