3763d1fe30173890dae20e5677316f1c26d38dec
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-06-06 10:26:53 -0700

    print function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * _putchar - print chara
+ * @c: character to print
+ * Return: always return 0
+ */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
