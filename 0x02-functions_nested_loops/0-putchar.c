faa8d596b846e6c04e97a7e81c9d6572e9df726c
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-06-06 10:00:42 -0700

    function prinsts putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @func - function prints _putchar
+ * @Return: return 0
+ */
+
+int func(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
