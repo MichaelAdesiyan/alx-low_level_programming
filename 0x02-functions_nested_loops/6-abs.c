82179d52d02cac3b269a4cee4d4c4ce14919760f
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-06-08 00:14:34 -0700

    function that computes te absolute alue of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @n - initialized argument
+ * Return: return an integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
