cb6defba71f8371b4c3e3bff6ee5f945c612129d
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-06-10 22:57:16 -0700

    function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * create_file - function that create a file 
- * @filename: file 
- * @text_content: file 
- * Return: 1, -1 
+ * create_file - function that create a file
+ * @filename: file
+ * @text_content: file
+ * Return: 1, -1
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -12,10 +12,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content)
-	{for (x = 0; text_content[x] != '\0'; x++);
-		star = write(folder, text_content, x);
-			if (star == -1)
-				return (-1);
+	{
+		for (x = 0; text_content[x] != '\0'; x++)
+		{
+			star = write(folder, text_content, x);
+		}
+		if (star == -1)
+			return (-1);
 	}
 	close(folder);
 	return (1);
