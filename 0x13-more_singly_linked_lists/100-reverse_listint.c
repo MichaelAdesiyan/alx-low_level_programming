0646cbcd7a01071e7710b95d9084b0af39306c86
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-06-13 13:46:35 -0700

    function that find the loop

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,27 +1,25 @@
 #include "lists.h"
 
 /**
- *  * reverse_listint - reverse linked list
- *   * @head: pointer to head pointer of linked list
- *    * Return: pointer to first node of reversed list
- *     */
-
-listint_t *reverse_listint(listint_t **head)
+ * reverse_listint - reverse linked list
+ * @head: pointer to head pointer of linked list
+ * Return: pointer to first node of reversed list
+ */
+size_t print_listint_safe(const listint_t *head)
 {
-		listint_t *prev, *next;
+	listint_t *prev, *next;
 
-			if (head == NULL || *head == NULL)
-						return (NULL);
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	prev = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
 
-				prev = NULL;
-					while (*head != NULL)
-							{
-										next = (*head)->next;
-												(*head)->next = prev;
-														prev = *head;
-																*head = next;
-																	}
-						(*head) = prev;
-
-							return (*head);
+	return (*head);
 }
