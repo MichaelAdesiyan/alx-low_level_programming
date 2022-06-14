6c212e77bc5877adc46d631b938d86002c8729f6
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-06-13 19:32:35 -0700

    function that adds a new node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,53 +1,46 @@
- #include "lists.h"
-
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
 /**
- *  * _strlen - find string length
- *   * @str: string
- *    * Return: length
- *     */
-int _strlen(const char *str)
-{
-		int i;
-
-			i = 0;
-				while (str[i] != '\0')
-							i++;
-					return (i);
-}
-
-/**
- *  * add_node_end - add node to end of linked list
- *   * @head: linked list
- *    * @str: data for new node
- *     * Return: address of new element, or NULL if failed
- *      */
+ * add_node_end - node add at  end of list
+ * @head: character to print
+ * @str: string for new node
+ * Return:  node
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-		list_t *new_node, *temp;
+	list_t *cur;
+	list_t *new_nd;
+	int c;
 
-			if (str == NULL)
-						return (NULL);
-				if (strdup(str) == NULL)
-							return (NULL);
+	cur = *head;
+	while (cur && cur->next != NULL)
+		cur = cur->next;
 
-					new_node = malloc(sizeof(list_t));
-						if (new_node == NULL)
-									return (NULL);
+	for (c = 0; str[c] != '\0'; c++)
+		;
+	new_nd = malloc(sizeof(list_t));
+	if (new_nd == NULL)
+	{
+		free(new_nd);
+		return (NULL);
+	}
+	new_nd->str = strdup(str);
+	if (new_nd->str == NULL)
+	{
+		free(new_nd);
+		return (NULL);
+	}
+	new_nd->str = strdup(str);
+	if (new_nd->str == NULL)
+	{
+		free(new_nd);
+		return (NULL);
+	}
+	new_nd->len = c;
+	new_nd->next = NULL;
 
-							new_node->str = strdup(str);
-								new_node->len = _strlen(str);
-									new_node->next = NULL;
-
-										if (*head == NULL)
-													*head = new_node;
-											else
-													{
-																temp = *head;
-
-																		while (temp->next != NULL)
-																						temp = temp->next;
-																				temp->next = new_node;
-																					}
-
-												return (new_node);
+	if (cur)
+		cur->next = new_nd;
+	return (new_nd);
 }
