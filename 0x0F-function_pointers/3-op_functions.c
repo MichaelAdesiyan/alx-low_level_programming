f017e585b874855128ec0e9b7ab721c231fc3a9b
MichaelAdesiyan <99549113+MichaelAdesiyan@users.noreply.github.com>
2022-06-15 03:53:25 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,49 +1,52 @@
-#include "3-calc.h"
 /**
- * op_add - addition operator
- * @a: first int
- * @b: second int
- * Return: sum of 2 numbers
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - subtraction operator
- * @a: first in
- * @b: second int
- * Return: difference of 2 numbers
+ * op_sub - subtract
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - product of 2 numbers
- * @a: first int
- * @b: second int
- * Return: product of the numbers
+ * op_mul - multiply
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - division of 2 numbers
- * @a: first int
- * @b: second int
- * Return: result of the division of the numbers
+ * op_div - divide
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
 /**
- * op_mod - get remainder of the division of 2 numbers
- * @a: first int
- * @b: second int
- * Return: remainder of division of the numbers
+ * op_mod - modulus for remainder
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_mod(int a, int b)
 {
