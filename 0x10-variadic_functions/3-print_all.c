2c474e294397e029602b64b16002c96ea88de2a0
MichaelAdesiyan <adesiyanoluwatobiloba3@gmail.com>
2022-06-16 17:02:54 -0700

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * print_int - prints int
  * @list: arguments from print_all
@@ -9,6 +10,7 @@ void print_int(va_list list)
 {
 	printf("%d", va_arg(list, int));
 }
+
 /**
  * print_float - prints float
  * @list: arguments from print_all
@@ -17,6 +19,7 @@ void print_float(va_list list)
 {
 	printf("%f", va_arg(list, double));
 }
+
 /**
  * print_char - prints int
  * @list: arguments from print_all
@@ -25,60 +28,56 @@ void print_char(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
+
 /**
  * print_str - prints string
  * @list: arguments from print_all
  */
 void print_str(va_list list)
 {
-	char *s;
-	s = va_arg(list, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
+	char *s = va_arg(list, char *);
+
+	s == NULL ? printf("(nil)") : printf("%s", s);
+
 }
 
 /**
  * print_all - prints any type
  * @format: arguments to print
  */
+
 void print_all(const char * const format, ...)
 {
-	
 	va_list list;
 	int i = 0, j = 0;
 	char *sep = "";
 
-	datatype choice[] = 
-	{
-		{'i', print_int},
-		{'f', print_float},
-		{'c', print_char},
-		{'s', print_str},
-		{'\0', NULL} 
+	printTypeStruct printType[] = {
+		{ "i", print_int },
+		{ "f", print_float },
+		{ "c", print_char },
+		{ "s", print_str },
+		{NULL, NULL}
 	};
 
 	va_start(list, format);
 
 	while (format && format[i])
+{
+	j = 0;
+	while (j < 4)
 	{
-		j = 0;
-		while (j < 4)
+		if (*printType[j].type == format[i])
 		{
-			if (choice[j].letter == format[i])
-			{
-				printf("%s", sep);
-				choice[j].printer(list);
-				sep = ", ";
-				break;
-			}
-			j++;
+			printf("%s", sep);
+			printType[j].printer(list);
+			sep = ", ";
+			break;
 		}
-		i++;
+		j++;
 	}
-	printf("\n");
-	va_end(list);
+	i++;
+}
+printf("\n");
+va_end(list);
 }
